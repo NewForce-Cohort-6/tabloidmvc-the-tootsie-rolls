6f1a9225988cd43c7499879ed5b0e180b1926170
Trey Chambers <trey.s.chambers@gmail.com>
2022-11-01 11:01:51 -0400

    Edit tag function working

--- TabloidMVC/Controllers/TagController.cs
+++ TabloidMVC/Controllers/TagController.cs
@@ -27,7 +27,12 @@ namespace TabloidMVC.Controllers
         // GET: TagController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            Tag tag = _tagRepo.GetTagById(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+            return View(tag);
         }
 
         // GET: TagController/Create
@@ -55,21 +60,28 @@ namespace TabloidMVC.Controllers
         // GET: TagController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Tag tag = _tagRepo.GetTagById(id);
+
+            if(tag == null)
+            {
+                return NotFound();
+            }
+            return View(tag);
         }
 
         // POST: TagController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Tag tag)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                _tagRepo.UpdateTag(tag);
+                return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                return View(tag);
             }
         }
 
